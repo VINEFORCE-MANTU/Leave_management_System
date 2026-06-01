284aa5ef51d8dcc20389bda96f2fd7fdbe0376bb
mantu kumar <sahharsh520@gmail.com>
2026-06-01 15:49:44 +0530

    solve status issue

--- 10.2.0/angular/src/app/leave/status/status.component.ts
+++ 10.2.0/angular/src/app/leave/status/status.component.ts
@@ -43,6 +43,7 @@ export class StatusComponent extends AppComponentBase {
   onSave = new EventEmitter<any>();
 
   statusList = [
+    { id: 1, name: 'Pending' },
     { id: 2, name: 'Approved' },
     { id: 3, name: 'Rejected' }
   ];
